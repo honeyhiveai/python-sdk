c3c319ca5abf793fce09526dd412038ec72dc2d0
michael-hhai <138688447+michael-hhai@users.noreply.github.com>
2023-11-16 15:16:09 -0500

    Fix namespace aliasing (#6)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ entry_points = {'console_scripts': ['honeyhive = honeyhive.cli:honeyhive']}
 
 setup(
     name='honeyhive',
-    version='0.1.66',
+    version='0.1.67',
     description='The HoneyHive SDK for Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
