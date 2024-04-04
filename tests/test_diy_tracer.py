741962b71fe5b90889bcdaf2ae1341acfb3a2bd2
michael-hhai <138688447+michael-hhai@users.noreply.github.com>
2024-04-04 11:34:20 -0400

    DIY tracer fixes (#17)

---
+++ tests/test_diy_tracer.py
@@ -0,0 +1,32 @@
+import os
+import honeyhive
+from honeyhive.utils.tracer import HoneyHiveTracer
+from openai import OpenAI
+
+openai_client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ["OPENAI_API_KEY"],
+)
+
+def test_thing():
+  tracer = HoneyHiveTracer(
+      project=os.environ["HH_PROJECT"],
+      name="DIY Test",
+      source="diy_test",
+      api_key=os.environ["HH_API_KEY"],
+  )
+  input = "Hello there"
+  with tracer.model(event_name="DIY Test", description="DIY testing", input=input, config={"provider": "openai"}) as model_call:
+    resp = openai_client.chat.completions.create(model="gpt-4", messages=[{"role": "user", "content": input}])
+
+def test_streaming_thing():
+  tracer = HoneyHiveTracer(
+      project=os.environ["HH_PROJECT"],
+      name="DIY Stream Test",
+      source="diy_stream_test",
+      api_key=os.environ["HH_API_KEY"],
+  )
+  input = "Hello there, stream"
+  with tracer.model(event_name="DIY Stream Test", description="DIY stream testing", input=input, config={"provider": "openai", "endpoint": "streaming"}) as model_call:
+    resp = openai_client.chat.completions.create(model="gpt-4", messages=[{"role": "user", "content": input}], stream=True)
+
