87d8968140eee4d0ce0cd544b4d9336fe5950227
Michael Acar <michael@honeyhive.ai>
2024-04-04 12:12:24 -0400

    Apply linting

--- tests/test_diy_tracer.py
+++ tests/test_diy_tracer.py
@@ -1,5 +1,4 @@
 import os
-import honeyhive
 from honeyhive.utils.tracer import HoneyHiveTracer
 from openai import OpenAI
 
@@ -8,26 +7,42 @@ openai_client = OpenAI(
     api_key=os.environ["OPENAI_API_KEY"],
 )
 
+
 def test_thing():
-  tracer = HoneyHiveTracer(
-      project=os.environ["HH_PROJECT"],
-      name="DIY Test",
-      source="diy_test",
-      api_key=os.environ["HH_API_KEY"],
-  )
-  input = "Hello there"
-  with tracer.model(event_name="DIY Test", description="DIY testing", input=input, config={"provider": "openai"}) as model_call:
-    resp = openai_client.chat.completions.create(model="gpt-4", messages=[{"role": "user", "content": input}])
-  assert model_call.event_id is not None
+    tracer = HoneyHiveTracer(
+        project=os.environ["HH_PROJECT"],
+        name="DIY Test",
+        source="diy_test",
+        api_key=os.environ["HH_API_KEY"],
+    )
+    input = "Hello there"
+    with tracer.model(
+        event_name="DIY Test",
+        description="DIY testing",
+        input=input,
+        config={"provider": "openai"},
+    ) as model_call:
+        resp = openai_client.chat.completions.create(
+            model="gpt-4", messages=[{"role": "user", "content": input}]
+        )
+    assert model_call.event_id is not None
+
 
 def test_streaming_thing():
-  tracer = HoneyHiveTracer(
-      project=os.environ["HH_PROJECT"],
-      name="DIY Stream Test",
-      source="diy_stream_test",
-      api_key=os.environ["HH_API_KEY"],
-  )
-  input = "Hello there, stream"
-  with tracer.model(event_name="DIY Stream Test", description="DIY stream testing", input=input, config={"provider": "openai", "endpoint": "streaming"}) as model_call:
-    resp = openai_client.chat.completions.create(model="gpt-4", messages=[{"role": "user", "content": input}], stream=True)
-  assert model_call.event_id is not None
+    tracer = HoneyHiveTracer(
+        project=os.environ["HH_PROJECT"],
+        name="DIY Stream Test",
+        source="diy_stream_test",
+        api_key=os.environ["HH_API_KEY"],
+    )
+    input = "Hello there, stream"
+    with tracer.model(
+        event_name="DIY Stream Test",
+        description="DIY stream testing",
+        input=input,
+        config={"provider": "openai", "endpoint": "streaming"},
+    ) as model_call:
+        resp = openai_client.chat.completions.create(
+            model="gpt-4", messages=[{"role": "user", "content": input}], stream=True
+        )
+    assert model_call.event_id is not None
