78bcba1776d8cd441875e900e0075b066303210e
Michael Acar <michael@honeyhive.ai>
2024-05-09 13:04:37 -0400

    Add config tests

--- tests/test_configurations.py
+++ tests/test_configurations.py
@@ -3,7 +3,9 @@ import os
 import uuid
 from honeyhive.models import components, operations
 
-sdk = honeyhive.HoneyHive(bearer_auth=os.environ["HH_API_KEY"])
+sdk = honeyhive.HoneyHive(
+    bearer_auth=os.environ["HH_API_KEY"], server_url=os.environ["HH_API_URL"]
+)
 
 
 def test_get_configurations():
@@ -15,14 +17,14 @@ def test_get_configurations():
 def test_post_configurations():
     random_string = str(uuid.uuid4())
     config_name = f"python-sdk-test-{random_string}"
-    configuration = components.Configuration(
+    configuration = components.PostConfigurationRequest(
         project=os.environ["HH_PROJECT_ID"],
         name=config_name,
         provider="test-provider",
-        parameters=components.Parameters(
-            call_type=components.CallType.CHAT, model="Test Model"
+        parameters=components.PostConfigurationRequestParameters(
+            call_type=components.PostConfigurationRequestCallType.CHAT,
+            model="Test Model",
         ),
-        type=components.Type.LLM,
     )
     res = sdk.configurations.create_configuration(configuration)
     assert res.status_code == 200
@@ -35,20 +37,29 @@ def test_post_configurations():
     assert res.configurations[0].id is not None
 
     inserted_id = res.configurations[0].id
-    configuration.type = components.Type.PIPELINE
     res = sdk.configurations.update_configuration(
-        id=inserted_id, configuration=configuration
+        id=inserted_id,
+        put_configuration_request=components.PutConfigurationRequest(
+            project=configuration.project,
+            name=configuration.name,
+            provider=configuration.provider,
+            parameters=configuration.parameters,
+            type=components.PutConfigurationRequestType.PIPELINE,
+        ),
     )
     assert res.status_code == 200
 
     res = sdk.configurations.get_configurations(
         project_name=os.environ["HH_PROJECT"],
         name=config_name,
-        type_=operations.Type.PIPELINE,
+        type_=operations.GetConfigurationsQueryParamType.PIPELINE,
     )
     assert res.status_code == 200
     assert len(res.configurations) == 1
-    assert res.configurations[0].type == components.Type.PIPELINE
+    assert (
+        res.configurations[0].type
+        == operations.GetConfigurationsQueryParamType.PIPELINE
+    )
 
     res = sdk.configurations.delete_configuration(id=inserted_id)
     assert res.status_code == 200
@@ -56,7 +67,7 @@ def test_post_configurations():
     res = sdk.configurations.get_configurations(
         project_name=os.environ["HH_PROJECT"],
         name=config_name,
-        type_=operations.Type.PIPELINE,
+        type_=operations.GetConfigurationsQueryParamType.PIPELINE,
     )
     assert res.status_code == 200
     assert len(res.configurations) == 0
