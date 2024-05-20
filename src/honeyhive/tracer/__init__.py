5de3345876cad2089ffd5efc0fcf21b62dd56ecf
Michael Acar <michael@honeyhive.ai>
2024-05-20 19:55:15 -0400

    Final fixes and test

--- src/honeyhive/tracer/__init__.py
+++ src/honeyhive/tracer/__init__.py
@@ -12,24 +12,23 @@ class HoneyHiveTracer:
         source,
         server_url="https://api.honeyhive.ai",
     ):
-        sdk = honeyhive.HoneyHive(bearer_auth=api_key, server_url=server_url)
-        res = sdk.session.start_session(
-            request=operations.StartSessionRequestBody(
-                session=components.SessionStartRequest(
-                    project=project,
-                    session_name=session_name,
-                    source=source,
+        try:
+            sdk = honeyhive.HoneyHive(bearer_auth=api_key, server_url=server_url)
+            res = sdk.session.start_session(
+                request=operations.StartSessionRequestBody(
+                    session=components.SessionStartRequest(
+                        project=project,
+                        session_name=session_name,
+                        source=source,
+                    )
                 )
             )
-        )
-        assert res.object.session_id is not None
-        session_id = res.object.session_id
-        Traceloop.init(
-            api_endpoint=f"{server_url}/opentelemetry",
-            headers={
-                "Authorization": f"Bearer {api_key}",
-                "session_id": session_id,
-                "project": project,
-                "source": source,
-            },
-        )
+            assert res.object.session_id is not None
+            session_id = res.object.session_id
+            Traceloop.init(
+                api_endpoint=f"{server_url}/opentelemetry",
+                app_name=session_id,
+                api_key=api_key,
+            )
+        except:
+            pass
