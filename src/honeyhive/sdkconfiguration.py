d3e694a7fe31856664fd8142562fd130fed34a57
SidPrak7 <sidharth@honeyhive.ai>
2024-10-01 20:04:03 -0400

    Session correction

--- src/honeyhive/sdkconfiguration.py
+++ src/honeyhive/sdkconfiguration.py
@@ -23,9 +23,9 @@ class SDKConfiguration:
     server_idx: Optional[int] = 0
     language: str = 'python'
     openapi_doc_version: str = '1.0.2'
-    sdk_version: str = '0.14.3'
+    sdk_version: str = '0.14.4'
     gen_version: str = '2.428.1'
-    user_agent: str = 'speakeasy-sdk/python 0.14.3 2.428.1 1.0.2 HoneyHive'
+    user_agent: str = 'speakeasy-sdk/python 0.14.4 2.428.1 1.0.2 HoneyHive'
     retry_config: Optional[RetryConfig] = None
 
     def __post_init__(self):
