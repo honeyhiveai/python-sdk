575bcd145bb68aaedcb3e7d98e581afd68b819ef
SidPrak7 <sidharth@honeyhive.ai>
2024-11-14 09:56:04 -0500

    Update version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ entry_points = {'console_scripts': ['honeyhive = honeyhive.cli:honeyhive']}
 
 setup(
     name='honeyhive',
-    version='0.2.23',
+    version='0.2.24',
     description='The HoneyHive SDK for Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
